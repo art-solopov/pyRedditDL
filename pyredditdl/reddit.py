1643dfec071a3d01c9b250dfc3a200a2b71b8f82
Artemiy Solopov <art-solopov@yandex.ru>
2015-01-18 02:31:40 +0300

    Added SQLite3 to Reddit for URL logging

--- pyredditdl/reddit.py
+++ pyredditdl/reddit.py
@@ -1,8 +1,26 @@
 import requests as req
 import requests.auth as reqa
 from copy import copy
+import sqlite3
+from datetime import date
+
+TABLE_NAME = 'py_reddit_dl_logs'
 
 class Reddit:
+
+    @classmethod
+    def init_log(cls, logpath):
+        cls.logdb = sqlite3.connect(logpath)
+        cr_cur = cls.logdb.cursor()
+        cr_cur.execute("SELECT sql FROM sqlite_master"
+                       "WHERE type='table' AND name=?",
+                       (TABLE_NAME,))
+        if len(cr_cur.fetchall()) == 0:
+            cr_cur.execute("CREATE TABLE " + TABLE_NAME +
+                           " (id INTEGER PRIMARY KEY, url VARCHAR(2000), date DATE")
+            cls.logdb.commit()
+
+
     def __init__(self, username, password, client_id, secret):
         self.username = username
         self.password = password
@@ -32,8 +50,19 @@ class Reddit:
 
     def get_obj_list(self, limit=1000):
         response = req.get('https://oauth.reddit.com/user/{0}/saved'.format(self.username), headers=self.headers)
-        # TODO logging, retrieval with actual limit
+        # TODO logging, retrieval with actual limit, checking for the processed links
         return response.json()['data']['children']
+
+    def write_last_processed(self, url):
+        cur = self.logdb.cursor()
+        cur.execute("INSERT INTO " + TABLE_NAME + " (url, date) VALUES (?, ?)", (url, date.today()))
+        self.logdb.commit()
+
+    def get_last_processed(self):
+        cur = self.logdb.cursor()
+        cur.execute("SELECT url FROM " + TABLE_NAME + " ORDER BY date DESC LIMIT 1")
+        return cur.fetchone()[0]
+
 
 class RedditAuthError(Exception):
     pass
